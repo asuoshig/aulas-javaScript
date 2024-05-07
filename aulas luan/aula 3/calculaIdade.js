8d06f1e701201408aa27457a3325a783a4d68279
geovana ferreira <144859297+asuoshig@users.noreply.github.com>
2024-05-07 14:43:14 -0300

    calculaIdade.js

--- aulas luan/aula 3/calculaIdade.js
+++ aulas luan/aula 3/calculaIdade.js
@@ -1,13 +1,27 @@
-const hoje =  new Date();
+//Crie uma expressão de função que calcule corretamente a idade de uma pessoa dada a data de aniversario sendo a string "dd/MM/yyyy" (d=dia com 2 digitos, M=mês com 2 digitos, y=ano com 4 digitos)
 
-console.log(hoje.getDate())
-console.log(hoje.getMonth())
-console.log(hoje.getFullYear())
+const idade = function(data){
+    const hoje = new Date();
 
-idade = function(data){
-    
-    
-    return dia
+console.log(`O dia de hoje é ${hoje.getDate()}`)
+console.log(`O mês é ${hoje.getMonth()}`)
+console.log(`E o ano é ${hoje.getFullYear()}`)
+
+const [dia, mes, ano] = data.split("/")
+
+let idade = hoje.getFullYear() - parseInt(ano)
+
+if(hoje.getMonth()< (parseInt(mes) - 1)){
+    return idade - 1
+}else if (hoje.getMonth() === (parseInt(mes) -1)){
+    if (hoje.getDate() < parseInt(dia)){
+        return idade -1
 }
 
-console.log(idade("26/09/1996"))
+    }
+    return idade;
+
+}
+console.log(idade('23/02/2005'));
+
+    
