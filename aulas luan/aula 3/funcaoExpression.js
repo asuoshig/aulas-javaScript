07ff00634b355b0d66625dee6f8b5759f60fee0c
geovana ferreira <144859297+asuoshig@users.noreply.github.com>
2024-05-13 07:42:19 -0300

    Add files via upload

--- aulas luan/aula 3/funcaoExpression.js
+++ aulas luan/aula 3/funcaoExpression.js
@@ -1,3 +1,5 @@
+//Utilizando-se das expressões de função, faça um programa que calcule a area de um circulo (Dica: const pi = Math.PI)
+
 const PI = Math.PI
 const area = function (PI, raio){
     return PI * (raio**2)
