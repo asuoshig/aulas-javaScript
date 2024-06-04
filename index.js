f1faab8dc62555c204c521f817ec5bad37353a91
geovana ferreira <144859297+asuoshig@users.noreply.github.com>
2024-06-04 13:40:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express')//trabalhar importaçao
+const boryParser = require ('body-parser');
 
+const PORTA = 3000
 const app = express()
+app.use(boryParser.json());
 
-app.listen(3000, 'localhost', () => {
+app.listen(3000, () => {
     console.log(`servidor rodando na porta http://localhost:3000`)
 })
 
 app.get('/', (req, res) => {
-    res.send('ola  mundo')
+   const mensagem = {
+    message: 'ola mundo',
+    to: 'aqui'
+   } //todas requisiçao tem uma resposta
+})
+
+app.post('/login', (req, res) => {
+    const{usuario, senha} = req.body
+    console.log(usuario)
+    console.log(senha)
+
+
+    if(usuario === 'usuario' & senha === 'senha'){
+        res.json({token: 'token'}) // token é fixa de credencial
+        // console.log('ok')
+    }else{
+        res.status(401).json({message: 'login invalido'})
+    }
+    
 })
