3e7be8a2d5961c725c296c9590ab908438984409
geovana ferreira <144859297+asuoshig@users.noreply.github.com>
2024-07-15 20:58:06 -0300

    Add files via upload

---
+++ javaScript/condição simples/index.js
@@ -0,0 +1,6 @@
+var velocidade = 34.9
+console.log(`a velocidade do seu carro é ${velocidade} km/h `)
+if(velocidade > 60){
+    console.log('você ultrapassou o limite de velocidade permitido. MULTADO!!')
+}
+console.log('dirija sempre com cinto de segurança segurança')
