72f3fd0e8e9e0f1d49b55766209ad71a3c0a96bd
geovana ferreira <144859297+asuoshig@users.noreply.github.com>
2024-07-21 15:22:39 -0300

    Add files via upload

---
+++ javaScript/condições aninhadas e multiplas/switch.js
@@ -0,0 +1,26 @@
+var agora = new Date()
+var diaSemana = agora.getDay()
+
+switch(diaSemana){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('segunda')
+        break
+    case 2:
+        console.log('terça')
+        break
+    case 3:
+        console.log('quarta')
+        break
+    case 4:
+        console.log('quinta')
+        break
+    case 5:
+        console.log('sexta')
+        break
+    case 6:
+        console.log('sábado')
+        break
+}
