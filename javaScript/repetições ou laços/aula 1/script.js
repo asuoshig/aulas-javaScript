2153d89472592021667a08cf322639239e34de56
geovana ferreira <144859297+asuoshig@users.noreply.github.com>
2024-07-23 08:42:32 -0300

    Add files via upload

---
+++ javaScript/repetições ou laços/aula 1/script.js
@@ -0,0 +1,17 @@
+
+//este executa o bloco e depois testa
+var contador = 1
+do{
+    console.log('bom dia') // vai repetir bom dia 10 vezes
+    contador++ // ++ é igual se escrevemos contador +1
+} while (contador <= 10)
+
+
+
+
+    //este testa e depois executa o bloco
+// var contador = 1
+// while (contador <= 10){
+//     console.log('bom dia') // vai repetir bom dia 10 vezes
+//     contador++ // ++ é igual se escrevemos contador +1
+// }
