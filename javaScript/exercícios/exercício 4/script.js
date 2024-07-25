aacb2bf900dcc7bd65ff9dd6b4190738c155582e
geovana ferreira <144859297+asuoshig@users.noreply.github.com>
2024-07-25 18:00:07 -0300

    Add files via upload

---
+++ javaScript/exercícios/exercício 4/script.js
@@ -0,0 +1,19 @@
+function calcular(){
+    let numero = document.getElementById('number')
+    let tab = document.getElementById('seltab')
+    if (numero.value.length == 0){
+        console.log('[ERRO] digite um número!')
+    }else{
+        let n = Number(numero.value)
+        let c = 1
+        tab.innerHTML = '' // limpa a tabuada antes de fazer a proxima
+        while (c <= 10){
+            let item = document.createElement('option')// cria uma option no select
+            item.text = `${n} X ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++ //incremento
+        }
+    }
+
+
+}
